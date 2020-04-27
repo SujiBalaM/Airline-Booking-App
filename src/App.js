c8ba6416e4b847197b625f7c7195da16e79a3cc8
smanoharan <smanoharan@empoweredbenefits.com>
2020-04-27 22:02:26 +0530

    material UI added and JSON server added

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+function App() {
+  return (
+    <div className='App'>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant='title' color='inherit'>
+            React & Material-UI Sample Application
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default App;
