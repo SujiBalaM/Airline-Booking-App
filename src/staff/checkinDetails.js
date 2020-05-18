8305c0b3853870364f563a2370a1f0aa8f252707
sujatha <sujathamanoharan7@gmail.com>
2020-05-18 20:57:51 +0530

    seat colors implemented

--- src/staff/checkinDetails.js
+++ src/staff/checkinDetails.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import Header from '../common/header';
+import { connect } from 'react-redux';
+import { dasboardData } from '../redux/dashboard/action';
 
 import Footer from '../common/footer';
 
@@ -9,41 +11,69 @@ class CheckinDetails extends Component {
     super(props);
     this.state = {
       points: 70,
+      flightId: props.match.params.flightNo,
+
     };
   }
+  componentDidMount() {
+    const { dasboardData } = this.props;
+    dasboardData();
+  }
 
-  getUserSeats() {
+  getUserSeats(totalSeats, checkedInPassangers) {
     let i = 0;
     let seats = [];
-    while (i < this.state.points) {
+    while (i < totalSeats) {
       i++;
+      let color = checkedInPassangers.map(n => n.seatnumber === `A${i}` ? true : false).filter(x => x);
+      console.log("color", color);
       seats.push(
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          width='40'
-          height='40'
-          viewBox='0 0 24 24'
-        >
-          <path d='M4 18v3h3v-3h10v3h3v-6H4zm15-8h3v3h-3zM2 10h3v3H2zm15 3H7V5c0-1.1.9-2 2-2h6c1.1 0 2 .9 2 2v8z' />
-        </svg>
+        <div style={{textAlign: "center"}}>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='40'
+            height='40'
+            viewBox='0 0 24 24' className={`seat-icon`}
+          >
+            <path d='M4 18v3h3v-3h10v3h3v-6H4zm15-8h3v3h-3zM2 10h3v3H2zm15 3H7V5c0-1.1.9-2 2-2h6c1.1 0 2 .9 2 2v8z' fill={`${color.length === 0 ? 'red' : 'green'}`} />
+          </svg>
+        <span>{`A${i}`}</span>
+        </div> 
       );
     }
     return seats;
   }
 
   render() {
+    const { dashboardList } = this.props;
+    console.log('from passenger details com', dashboardList);
+    const flightId = this.state.flightId;
+    const checkinDetails = dashboardList.data.filter(
+      (data) => data.flightNo === flightId
+    );
+    console.log('checkinDetails------>', checkinDetails);
+
     return (
       <div>
         <Header />
         <br />
         <h4>Passenger Checked In Details</h4>
         <br />
-        <div>{this.getUserSeats(this.state.points)}</div>
+        <div className="seats">{this.getUserSeats(this.state.points, checkinDetails)}</div>
 
         <Footer />
       </div>
     );
   }
 }
+const mapStateToProps = (state) => {
+  return {
+    dashboardList: state.dashboardAdminList,
+  };
+};
 
-export default CheckinDetails;
+const mapDispatchToProps = (dispatch) => ({
+  dasboardData: () => dispatch(dasboardData()),
+});
+
+export default (connect(mapStateToProps,mapDispatchToProps)(CheckinDetails));
